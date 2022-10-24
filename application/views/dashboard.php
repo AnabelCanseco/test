f1e9c38179e3250b8a831f75710e9354aebf8dc2
Anabel Canseco <anabel.canseco@gmail.com>
2022-10-24 16:57:00 -0500

    Dashboard view

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -5,44 +5,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Test</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
 	<link rel="stylesheet"  href="<?= base_url('assets/css/login.css?v=' . rand()) ?>">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.5/jquery.validate.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 <body class="bg-light">
-	<div class="container mt-5">
-		<div class="card  mt-5 mb-3" >
-		  <div class="row g-0">
-		    <div class="col-md-6">
-		      <img src="<?php echo base_url('assets/img/login.jpg') ?>" class="img-fluid rounded-start" alt="...">
-		    </div>
-		    <div class="col-md-6 bg-login" >
-		      <div class="card-body mt-5">
-		      	<div class="col-md-12 mb-5">
-			  		<h4 class="text-center">Inicio de sesión</h4>
-			  	</div>
-				<form class="row g-4 mt-5" id="login-form" method="post">
-					<div class="col-md-12 col-sm-12">
-						<label for="email" class="form-label">Email</label>
-						<input type="email" class="form-control" id="email" name="email">
-						<span class="text-danger" id="email-error"></span>
-					</div>
-					<div class="col-md-12 col-sm-12">
-						<label for="pwd" class="form-label">Contraseña</label>
-						<input type="password" class="form-control" id="pwd" name="pwd">
-						<span class="text-danger" id="pwd-error"></span>
-					</div>
-					<div class="col-12 text-center">
-						<button type="submit" class="btn btn-lg btn-primary ">Iniciar sesión</button>
-					</div>
-				</form>
-				</div>
-
-		    </div>
-		  </div>
-		</div>
+	<div>
+		<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Dashboard test</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dashboard test</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="<?php echo base_url("dashboard") ?>">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url('login/destroy') ?>">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
 	</div>
+	<div></div>
 	<script src="<?php echo base_url('assets/js/login.js') ?>"></script>
 </body>
 </html>
